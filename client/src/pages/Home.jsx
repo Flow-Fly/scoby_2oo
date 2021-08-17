77493c452b4de35f1eb4cae4373f1edf1ab2b447
Flow-Fly <florian.aube@gmail.com>
2021-08-17 18:01:38 +0200

    youhou

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Home = (props) => {
-  // Implement react map box here.
-  return (
-    <div>
-      <h1>MAPBOX MAP HERE</h1>
-      <p>On home /</p>
-    </div>
-  );
-};
+import apiHandler from '../api/apiHandler';
+
+const Map = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+});
+
+class Home extends React.Component {
+  state = {
+    items: null,
+  };
+
+  async componentDidMount() {
+    try {
+      const { data } = await apiHandler.getItems;
+      console.log("datat received",data)
+      this.setState({
+        items: data,
+      })
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    console.log(this.state.items)
+    if (this.state.items === null)
+      return <div className="loading">Loading...</div>;
+    return (
+      <div>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: '100vh',
+            width: '100vw',
+          }}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ 'icon-image': 'marker-15' }}
+          >
+            <Feature coordinates={[48.852209, 2.355411]} />
+          </Layer>
+        </Map>
+        ;
+      </div>
+    );
+  }
+}
 
 export default Home;
